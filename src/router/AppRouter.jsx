0ea4cbceba56eff4dbc2ea0dff9d9fbbd640d595
kevin joel noviello <kjnoviello@gmail.com>
2024-07-08 17:11:32 -0300

    Route[opportunities] add page opportunities to route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { LandingPage, PrivacyPolicy, AboutUs, Login, Register, Opportunities } from "../pages"
-import ProyectCard from "../components/ProyectCard/ProyectCard"
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-             <Route path="/home" element={<LandingPage />} /> 
-             <Route path="/*" element={<LandingPage />} /> 
-             <Route path="/about" element={<AboutUs />} /> 
-             <Route path="/privacy" element={<PrivacyPolicy />} /> 
-             <Route path="/login" element={<Login />} /> 
-             <Route path="/register" element={<Register />} />
-             <Route path="/opportunities" element={<Opportunities />} />
-             <Route path="/proyectcard" element={<ProyectCard />} />
-        </Routes>
+      <Routes>
+        <Route path="/home" element={<LandingPage />} /> 
+        <Route path="/*" element={<LandingPage />} /> 
+        <Route path="/about" element={<AboutUs />} /> 
+        <Route path="/opportunities" element={<Opportunities />} /> 
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} />
+        <Route path="/opportunities" element={<Opportunities />} />
+      </Routes>
     </>
   )
 }
