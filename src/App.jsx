167eb682cee3102afd8bd45f1af530a92419c854
lucas.nunez@7r1ck.com <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>
2024-07-15 19:45:45 -0300

    footer mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
 import { Navbar, Footer } from "./pages";
+import ScrollToTop from './components/Scrolltop/ScrollToTop';
 import "./App.css";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar />
+        <ScrollToTop />
         <div className="main-content">
           <AppRouter />
         </div>
