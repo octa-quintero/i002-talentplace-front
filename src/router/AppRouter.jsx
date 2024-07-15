fbe4eba57ce0ae07929037efcae4b586f853631b
julimamani <julietamamani47@gmail.com>
2024-07-15 11:40:14 -0300

    opportunities-oportunidades

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,31 +1,11 @@
 import { Route, Routes } from "react-router-dom"
-/* jm-navbar
-import { LandingPage, PrivacyPolicy, AboutUs, Login, Register,Contratar,Oportunidades,ContactUs} from "../pages"
-*/
 
-import { LandingPage, PrivacyPolicy, AboutUs, Login, Register, Opportunities } from "../pages"
+import { LandingPage, PrivacyPolicy, AboutUs, Login, Register, Opportunities, Contratar} from "../pages"
 
 
 export const AppRouter = () => {
   return (
     <>
-
-    /*jm-navbar
-        <Routes>
-             <Route path="/home" element={<LandingPage />} /> 
-             <Route path="/*" element={<LandingPage />} /> 
-    
-             <Route path="/nosotros" element={<AboutUs />} /> 
-             <Route path="/contratar" element={<Contratar/>} /> 
-             <Route path="/oportunidades" element={<Oportunidades />} /> 
-             <Route path="/contacto" element={<ContactUs />} /> 
-  
-             <Route path="/privacy" element={<PrivacyPolicy />} /> 
-             <Route path="/ingreso" element={<Login />} /> 
-             <Route path="/register" element={<Register />} /> 
-        </Routes>
-    */
-
       <Routes>
         <Route path="/home" element={<LandingPage />} /> 
         <Route path="/*" element={<LandingPage />} /> 
