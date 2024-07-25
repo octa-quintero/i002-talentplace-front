fdd4de5ad2d9267fcb8a5ce8a88d6a9db3cb0a6d
XabierGallardo <javiergallard91@gmail.com>
2024-07-25 16:37:54 -0300

    Rendering freelancer's project data on main dashboard & postulaciones

--- src/components/DashboardFreelancer/DashboardFreelancer.jsx
+++ src/components/DashboardFreelancer/DashboardFreelancer.jsx
@@ -1,14 +1,14 @@
 import { Card, Row, Col } from "react-bootstrap";
-import { useAllProjectsByUserId } from '../../hooks/useAllProjectsByUserId';
 import formatDateLocal from "../../utils/formatDateLocal";
 import Loading from "../../pages/shared/Loading";
+import { useFetchApplicationsById } from "../../hooks/useFetchApplicationsById";
 
 
 export const DashboardFreelancer = () => {
 
-    let saldoFreelancer = "USD 800,00";
+    const { applications, projects, loading } = useFetchApplicationsById();
 
-    const { projects, loading } = useAllProjectsByUserId();
+    console.log(projects)
 
     const stateProjectParse = (boolean) => {
         return boolean ? "Vigente" : "No vigente";
@@ -19,98 +19,100 @@ export const DashboardFreelancer = () => {
         const restText = modalidad.slice(1);
         return firstLetter + restText;
     }
-
+    const tipoPrecio = "Fijo";
+    const saldoFreelancer = "USD 800,00";
 
     return (
-        <>
-            <div className="p-3 mt-5">
-                <h3 className='fs-2 mx-3'>Estado de la cuenta</h3>
-                <Card className="border border-info align-items-end m-3 p-3" >
 
-                    <Row>
-                        <Col lg={6}>
-                            <Card.Title className="fw-bold">Proyectos activos</Card.Title>
-                            <Card.Text className=''>{ projects.length }</Card.Text>
-                        </Col>
+        <div className="p-3 mt-5">
+            <h3 className='fs-2 mx-3'>Estado de la cuenta</h3>
+            <Card className="border border-info align-items-end m-3 p-3" >
 
-                        <Col lg={6}>
-                            <Card.Title className='fw-bold'>Saldo disponible</Card.Title>
-                            <Card.Text>{saldoFreelancer}</Card.Text>
-                        </Col>
+                <Row>
+                    <Col lg={6}>
+                        <Card.Title className="fw-bold">Proyectos activos</Card.Title>
+                        <Card.Text className=''>{ loading ? 0 : projects.length }</Card.Text>
+                    </Col>
 
-                    </Row>
+                    <Col lg={6}>
+                        <Card.Title className='fw-bold'>Saldo disponible</Card.Title>
+                        <Card.Text>{saldoFreelancer}</Card.Text>
+                    </Col>
 
-                </Card>
-            </div>
+                </Row>
+
+            </Card>
+
             {loading ? (
                 <div className="loading"><Loading /></div>
             ) :
+                projects.map((project) => (
 
-                <div className="p-3">
-                    <h3 className='p-3 fs-2'>Última postulación</h3>
-                    {projects.map((project) => (
-                        <Card key={project.id} className="align-items-start m-3 border border-info">
-                            <Card.Body className="p-3">
+                    <Card key={project.id} className="align-items-start m-3 border border-info p-2 m-1 rounded-4 pt-4 card-postulacion">
+
+                        <Card.Body className="pt-0">
+                            <Row className="">
                                 <Row>
-                                    <Card.Title className="fw-bold">Proyecto:</Card.Title>
-                                    <Card.Text>{project.titulo}</Card.Text>
+                                    <Card.Title className="fw-bold">{project.titulo}</Card.Title>
+                                </Row>
+
+                                <Row>
+                                    <Card.Text className="pt-3">{project.descripcion}</Card.Text>
                                 </Row>
 
                                 <Row className="pt-3">
-                                    <Col lg={4}>
-                                        <Card.Title className="fw-bold">Último pago efectuado</Card.Title>
-                                        <Card.Text>{formatDateLocal(project.fechaCreacion)}</Card.Text>
-                                    </Col>
-                                    <Col lg={4}>
-                                        <Card.Title className="fw-bold">Estado:</Card.Title>
-                                        <Card.Text>{stateProjectParse(project.estado)}</Card.Text>
-                                    </Col>
-                                    <Col lg={4}>
-                                        <Card.Title className="fw-bold">Total hasta el momento:</Card.Title>
-                                        <Card.Text>{projects.length}</Card.Text>
-                                    </Col>
 
-                                    <Row className="pt-3">
-
-                                        <Col lg={3}>
-                                            <Row className="text-center px-1">
-                                                <Col className="p-0 m-1">
-                                                    <Card.Title className="fw-bold p-1 m-1 text-end">
-                                                        Modalidad:
-                                                    </Card.Title>
-                                                </Col>
-                                                <Col className="p-0 m-1 rounded-4">
-                                                    <Card.Text className="p-1 m-1 text-start">
-                                                        {firstToUpperCase(project.modalidad)}
-                                                    </Card.Text>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                        <Col lg={3}>
-                                            <Row className="text-center px-1">
-                                                <Col className="p-0 m-1">
-                                                    <Card.Title className="fw-bold p-1 m-1 text-end">
-                                                        Estado:
-                                                    </Card.Title>
-                                                </Col>
-                                                <Col className="p-0 m-1 rounded-4">
-                                                    <Card.Text className="p-1 m-1 text-start">
-                                                        {stateProjectParse(project.estado)}
-                                                    </Card.Text>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                    </Row>
+                                    <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
+                                        <Row className="text-center px-1">
+                                            <Col className="p-0 m-1">
+                                                <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                    Estado
+                                                </Card.Title>
+                                            </Col>
+                                            <Col className="p-0 m-1 rounded-4">
+                                                <Card.Text className="p-1 m-1 text-start">
+                                                    {stateProjectParse(project.estado)}
+                                                </Card.Text>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
+                                        <Row className="text-center px-1">
+                                            <Col className="p-0 m-1">
+                                                <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                    Publicado
+                                                </Card.Title>
+                                            </Col>
+                                            <Col className="p-0 m-1 rounded-4">
+                                                <Card.Text className="p-1 m-1 text-start">
+                                                    {firstToUpperCase(project.modalidad)}
+                                                </Card.Text>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={4}>
+                                        <Row className="text-center px-1">
+                                            <Col className="p-0 m-1">
+                                                <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                    Tipo de precio
+                                                </Card.Title>
+                                            </Col>
+                                            <Col className="p-0 m-1 rounded-4">
+                                                <Card.Text className="p-1 m-1 text-start">
+                                                    {tipoPrecio}
+                                                </Card.Text>
+                                            </Col>
+                                        </Row>
+                                    </Col>
                                 </Row>
-                            </Card.Body>
-                        </Card>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                )
+                )
 
-                    ))[0]
-                    }
-                </div>
             }
-
-        </>
+        </div>
     );
 };
 
--- src/components/Postulaciones/Postulaciones.jsx
+++ src/components/Postulaciones/Postulaciones.jsx
@@ -1,112 +1,5 @@
-// import { Card, Col, Row } from "react-bootstrap"
-// import { useAllProjectsByUserId } from "../../hooks/useAllProjectsByUserId";
-// import Loading from "../../pages/shared/Loading";
-// import './Postulaciones.css';
-
-
-// export const Postulaciones = () => {
-
-//     // const { projects, loading } = useAllProjectsByUserId();
-//     console.log(projects)
-
-//     const stateProjectParse = (boolean) => {
-//         return boolean ? "Vigente" : "No vigente";
-//     }
-
-//     const firstToUpperCase = (modalidad) => {
-//         const firstLetter = modalidad.charAt(0).toUpperCase();
-//         const restText = modalidad.slice(1);
-//         return firstLetter + restText;
-//     }
-
-//     return (
-//         <>
-//             {loading ? (
-//                 <div className="loading"><Loading /></div>
-//             ) :
-
-//                 <div className="p-3 my-5">
-
-//                     {projects.map((project) => (
-
-//                         <Card key={project.id} className="align-items-start m-3 border border-info p-2 m-1 rounded-4 pt-4 card-postulacion">
-
-//                             <Card.Body className="pt-0">
-//                                 <Row className="">
-//                                     <Row>
-//                                         <Card.Title className="fw-bold">{project.titulo}</Card.Title>
-//                                     </Row>
-
-//                                     <Row>
-//                                         <Card.Text className="pt-3">{project.descripcion}</Card.Text>
-//                                     </Row>
-
-//                                     <Row className="pt-3">
-
-//                                         <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
-//                                             <Row className="text-center px-1">
-//                                                 <Col className="p-0 m-1">
-//                                                     <Card.Title className="fw-bold p-1 m-1 text-end">
-//                                                         Estado
-//                                                     </Card.Title>
-//                                                 </Col>
-//                                                 <Col className="p-0 m-1 rounded-4">
-//                                                     <Card.Text className="p-1 m-1 text-start">
-//                                                         {stateProjectParse(project.estado)}
-//                                                     </Card.Text>
-//                                                 </Col>
-//                                             </Row>
-//                                         </Col>
-//                                         <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
-//                                             <Row className="text-center px-1">
-//                                                 <Col className="p-0 m-1">
-//                                                     <Card.Title className="fw-bold p-1 m-1 text-end">
-//                                                         Publicado
-//                                                     </Card.Title>
-//                                                 </Col>
-//                                                 <Col className="p-0 m-1 rounded-4">
-//                                                     <Card.Text className="p-1 m-1 text-start">
-
-//                                                         {firstToUpperCase(project.modalidad)}
-//                                                     </Card.Text>
-//                                                 </Col>
-//                                             </Row>
-//                                         </Col>
-//                                         <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={4}>
-//                                             <Row className="text-center px-1">
-//                                                 <Col className="p-0 m-1">
-//                                                     <Card.Title className="fw-bold p-1 m-1 text-end">
-//                                                         Tipo de precio
-//                                                     </Card.Title>
-//                                                 </Col>
-//                                                 <Col className="p-0 m-1 rounded-4">
-//                                                     <Card.Text className="p-1 m-1 text-start">
-//                                                         {stateProjectParse(project.estado)}
-//                                                     </Card.Text>
-//                                                 </Col>
-//                                             </Row>
-//                                         </Col>
-//                                     </Row>
-//                                 </Row>
-//                             </Card.Body>
-//                         </Card>
-
-//                     ))
-//                     }
-//                 </div>
-//             }
-//         </>
-//     );
-// };
-
-// export default Postulaciones;
-
-
-
-
-
+import React, { useEffect, useState } from 'react';
 import { Card, Col, Row } from "react-bootstrap"
-import { useAllProjectsByUserId } from "../../hooks/useAllProjectsByUserId";
 import Loading from "../../pages/shared/Loading";
 import './Postulaciones.css';
 import { useFetchApplicationsById } from "../../hooks/useFetchApplicationsById";
@@ -114,103 +7,93 @@ import { useFetchApplicationsById } from "../../hooks/useFetchApplicationsById";
 
 export const Postulaciones = () => {
 
-    const {application, loading} = useFetchApplicationsById();
-    console.log(application);
+    const { applications, projects, loading } = useFetchApplicationsById();
 
+    const stateProjectParse = (boolean) => {
+        return boolean ? "Vigente" : "No vigente";
+    }
 
+    const firstToUpperCase = (modalidad) => {
+        const firstLetter = modalidad.charAt(0).toUpperCase();
+        const restText = modalidad.slice(1);
+        return firstLetter + restText;
+    }
 
-    //! HAY QUE HACER UN FETCH
-    //! LUEGO UN HOOK CON ESE FETCH
-    //! Y MAPEARLO EN ESTE COMPONENTE
-    //TODO este componente muestra los proyectos a los cuales el usuario se postula
-
-    // const { projects, loading } = useAllProjectsByUserId();
-    // console.log(projects)
-
-    // const stateProjectParse = (boolean) => {
-    //     return boolean ? "Vigente" : "No vigente";
-    // }
-
-    // const firstToUpperCase = (modalidad) => {
-    //     const firstLetter = modalidad.charAt(0).toUpperCase();
-    //     const restText = modalidad.slice(1);
-    //     return firstLetter + restText;
-    // }
+    const tipoPrecio = "Fijo";
 
     return (
         <>
-            {/* {loading ? (
-                <div className="loading"><Loading /></div>
-            ) : */}
+            {
+                loading ? (
+                    <div className="loading" > <Loading /></div >
+                ) :
+                    projects.map((project) => (
+                        <div className="p-3 my-5">
 
-                <div className="p-3 my-5">
+                            <Card key={project.id} className="align-items-start m-3 border border-info p-2 m-1 rounded-4 pt-4 card-postulacion">
 
+                                <Card.Body className="pt-0">
+                                    <Row className="">
+                                        <Row>
+                                            <Card.Title className="fw-bold">{project.titulo}</Card.Title>
+                                        </Row>
 
-                        <Card className="align-items-start m-3 border border-info p-2 m-1 rounded-4 pt-4 card-postulacion">
+                                        <Row>
+                                            <Card.Text className="pt-3">{project.descripcion}</Card.Text>
+                                        </Row>
 
-                            <Card.Body className="pt-0">
-                                <Row className="">
-                                    <Row>
-                                        <Card.Title className="fw-bold">project.titulo</Card.Title>
+                                        <Row className="pt-3">
+
+                                            <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
+                                                <Row className="text-center px-1">
+                                                    <Col className="p-0 m-1">
+                                                        <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                            Estado
+                                                        </Card.Title>
+                                                    </Col>
+                                                    <Col className="p-0 m-1 rounded-4">
+                                                        <Card.Text className="p-1 m-1 text-start">
+                                                            {stateProjectParse(project.estado)}
+                                                        </Card.Text>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
+                                                <Row className="text-center px-1">
+                                                    <Col className="p-0 m-1">
+                                                        <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                            Publicado
+                                                        </Card.Title>
+                                                    </Col>
+                                                    <Col className="p-0 m-1 rounded-4">
+                                                        <Card.Text className="p-1 m-1 text-start">
+                                                            {firstToUpperCase(project.modalidad)}
+                                                        </Card.Text>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={4}>
+                                                <Row className="text-center px-1">
+                                                    <Col className="p-0 m-1">
+                                                        <Card.Title className="fw-bold p-1 m-1 text-end">
+                                                            Tipo de precio
+                                                        </Card.Title>
+                                                    </Col>
+                                                    <Col className="p-0 m-1 rounded-4">
+                                                        <Card.Text className="p-1 m-1 text-start">
+                                                            {tipoPrecio}
+                                                        </Card.Text>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
                                     </Row>
-
-                                    <Row>
-                                        <Card.Text className="pt-3">project.descripcion</Card.Text>
-                                    </Row>
-
-                                    <Row className="pt-3">
-
-                                        <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
-                                            <Row className="text-center px-1">
-                                                <Col className="p-0 m-1">
-                                                    <Card.Title className="fw-bold p-1 m-1 text-end">
-                                                        Estado
-                                                    </Card.Title>
-                                                </Col>
-                                                <Col className="p-0 m-1 rounded-4">
-                                                    <Card.Text className="p-1 m-1 text-start">
-                                                        project.estado
-                                                    </Card.Text>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                        <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={3}>
-                                            <Row className="text-center px-1">
-                                                <Col className="p-0 m-1">
-                                                    <Card.Title className="fw-bold p-1 m-1 text-end">
-                                                        Publicado
-                                                    </Card.Title>
-                                                </Col>
-                                                <Col className="p-0 m-1 rounded-4">
-                                                    <Card.Text className="p-1 m-1 text-start">
-
-                                                        project.modalidad
-                                                    </Card.Text>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                        <Col className="border border-1 p-0 m-1 rounded-4 card-border" lg={4}>
-                                            <Row className="text-center px-1">
-                                                <Col className="p-0 m-1">
-                                                    <Card.Title className="fw-bold p-1 m-1 text-end">
-                                                        Tipo de precio
-                                                    </Card.Title>
-                                                </Col>
-                                                <Col className="p-0 m-1 rounded-4">
-                                                    <Card.Text className="p-1 m-1 text-start">
-                                                        project.estado
-                                                    </Card.Text>
-                                                </Col>
-                                            </Row>
-                                        </Col>
-                                    </Row>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-
-                    
-                </div>
-            {/* } */}
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    )
+                )
+            }
         </>
     );
 };
